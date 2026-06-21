4dc800cb816e8f7f7e86eaa19690c81962d803f6
Aqdas Amir <aqdasamir333@gmail.com>
2026-06-21 16:31:24 +0530

    Integrated Google GenAI And added AI service for contenet generation with file upload

---
+++ backend/src/routes/interview.routes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const { authUser } = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/file.middleware");
+const { generateInterviewReportController } = require("../controllers/interview.controller");
+const interviewRouter = express.Router()
+
+/**
+ * @route POST /api/interview/
+ * @description generate new interview report on the basis of user self description,resume pdf and job description.
+ * @access private
+ */
+interviewRouter.post("/", authUser, upload.single("resume"), generateInterviewReportController);
+
+module.exports = interviewRouter
