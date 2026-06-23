85c86e5394ff9f819738b1518919a05affb79c44
Aqdas Amir <aqdasamir333@gmail.com>
2026-06-24 01:05:22 +0530

    solved the cross-origin cookie blocking

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,13 @@ async function registerUserController(req, res) {
         });
 
         const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
-        res.cookie('token', token)
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        });
+        
         res.status(201).json({
              message: 'User registered successfully', 
             user: {
@@ -64,7 +70,12 @@ async function loginUserController(req, res) {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
         const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
-        res.cookie('token', token);
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        });
         res.status(200).json({
             message: 'User logged in successfully',
             user: {
@@ -91,11 +102,12 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({ token })
     }
 
-    res.clearCookie("token")
-
-    res.status(200).json({
-        message: "User logged out successfully"
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
     })
+    res.status(200).json({ message: "User logged out successfully" })
 }
 
 /**
